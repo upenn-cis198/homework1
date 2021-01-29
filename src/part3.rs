30c25d64ae026273551b80fa304324bd50f634f8
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-01-28 21:53:22 -0500

    part 3 updates

--- src/part3.rs
+++ src/part3.rs
@@ -1,23 +1,42 @@
+/*
+    CIS198 Homework 1
+    Part 3: Ownership, move semantics, and lifetimes
 
-// Part 3.
-// Lifetimes and move semantics.
+    Complete and write at least one unit test for each function you implement.
+    If it already has a unit test, either add assertions to it or add a new one.
+    Also answer the questions in text.
+*/
 
-// Problem 1
+// Remove these once you are done editing the file!
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+/*
+    Problem 1: Swap ints
+
+    Implement the function that swaps two integers, and write unit tests.
+
+    Make sure you write a test for when x1 and x2 are the same!
+
+    The Rust borrow checker may help avoid some possible bugs.
+*/
 pub fn swap_ints(x1: &mut i32, x2: &mut i32) {
     unimplemented!()
 }
 
-// Problem 1.
-// What went wrong? Copy strings properly.
-#[test]
-fn copy_string_test() {
-    let str1 = String::from("foo");
-    let str2 = str1;
-    assert_eq!(str1, str2);
-}
+/*
+    Problem 2: String duplication
+*/
+// #[test]
+// fn copy_string_test() {
+//     let str1 = String::from("foo");
+//     let str2 = str1;
+//     assert_eq!(str1, str2);
+// }
+// This test doesn't work. Fix it by copying strings properly.
+// Q1. What went wrong?
 
-// Problem 2.
-// Question 2: How come it works fine here?
+// Q2. How come it works fine here?
 #[test]
 fn copy_int_test() {
     let i1 = 1;
@@ -25,81 +44,173 @@ fn copy_int_test() {
     assert_eq!(i1, i2);
 }
 
-// Problem 3.
-// These two don't work either. Fix by changing the type of "string" in the function
-// copy_me ONLY, and by adjusting the parameter to "copy_me" where it's called.
-#[test]
-fn eat_me_test() {
-    let str1 = String::from("foo");
-    assert_eq!(str1, copy_me(/* Change in here only*/ str1));
+// Now implement the following function that duplicates a string n times.
+fn duplicate_string(s: &str, times: usize) -> Vec<String> {
+    unimplemented!()
 }
 
-#[test]
-fn eat_me_test2() {
-    let str1 = String::from("foo");
-    let str2 = copy_me(str1 /* Change in here only*/);
-    assert_eq!(str1, str2);
-}
+/*
+    Problem 3: String duplication continued
 
-fn copy_me(string : /* Change in here only*/ String) -> String {
-    string.clone()
-}
+    These two don't work either. Fix by changing the type of "string" in the
+    function copy_me ONLY, and by adjusting the parameter to "copy_me" where
+    it's called.
+*/
 
-// Problem 4.
-// Can you implement this function?
-// Add a lifetime specifier to the return type if you think it will help.
-// If not, why not.
-// fn new_ref_string() -> & String {
+// fn copy_me(string: /* Change in here only*/ String) -> String {
+//     string.clone()
+// }
+
+// #[test]
+// fn copy_me_test() {
+//     let str1 = String::from("foo");
+//     assert_eq!(str1, copy_me(/* Change in here only*/ str1));
+// }
+
+// #[test]
+// fn copy_me_test2() {
+//     let str1 = String::from("foo");
+//     let str2 = copy_me(str1 /* Change in here only*/);
+//     assert_eq!(str1, str2);
+// }
+
+/*
+    Problem 4: Lifetime specifiers
+
+    For each of the following three functions, either implement it by adding
+    lifetime specifiers, or explain why this is not possible.
+
+    (It's not truly impossible -- we will see later on that advanced features
+    such as "unsafe code" can be used to turn off Rust's safety and lifetime
+    checks.)
+*/
+// fn new_ref_string() -> &String {
 //     unimplemented!();
 // }
 
-// Problem 5.
-// Can you implement this function?
-// Add a lifetime specifier to the return type if you think it will help.
-// If not, why not.
-// fn new_ref_str() -> & str {
+// fn new_ref_str() -> &str {
 //     unimplemented!();
 // }
 
-// Problem 6.
-// Now we know how to implement this type of function. Implement it and write a test
-// pick_longest_tests2() which passes all tests.
-// fn pick_longest2(s1: & str, s2: & str) -> & str{
+// The same function from part2
+// fn pick_longest2(s1: &str, s2: &str) -> &str {
 //     unimplemented!()
 // }
 
-// Problem 7.
-// Write a function with a type signature which type checks the following test:
-// and passes the test.
-// This function compares it's second argument againsts all elements in it's first
-// argument, returning those that are less than (<).
-// fn find_lesser_values_test() {
-//     assert_eq!(find_lesser_values(& vec!["foo", "bar", "foobar"], "zzzzzzzz"),
-//                vec!["foo", "bar", "foobar"]);
-//     assert_eq!(find_lesser_values(& vec!["foo", "bar", "foobar"], "bars"),
-//                vec!["bar"]);
-//     // Add more tests.
-// }
+/*
+    Problem 5: Using functions with lifetimes
 
-// Problem 8
-// Move semantics present intersting API design choices not found in other languages.
-// HashMap is an example of such a API.
-// https://doc.rust-lang.org/std/collections/struct.HashMap.html
+    Write two versions of a function which returns the longest string in a
+    vector, using pick_longest2 as a helper function.
 
-// Specifically, the Entry API:
-// https://doc.rust-lang.org/std/collections/hash_map/enum.Entry.html
+    You can add lifetime specifiers if necessary.
+    What are the pros and cons of v1 and v2?
+*/
+
+fn pick_longest_in_v1(v: Vec<String>) -> String {
+    unimplemented!()
+}
+
+fn pick_longest_in_v2(v: Vec<&str>) -> &str {
+    unimplemented!()
+}
+
+/*
+    Problem 6: Move semantics
+
+    Write three versions of a function that pads a vector with zeros.
+    Fail if the vector is larger than the desired length.
+
+    Use .clone() as necessary to make the unit tests compile.
+
+    Which of these functions do you prefer? Which is the most efficient?
+*/
+
+fn pad_with_zeros_v1(v: Vec<usize>, desired_len: usize) -> Vec<usize> {
+    unimplemented!()
+    // debug_assert_eq!(result.len(), desired_len);
+}
+
+fn pad_with_zeros_v2(slice: &[usize], desired_len: usize) -> Vec<usize> {
+    unimplemented!()
+    // debug_assert_eq!(result.len(), desired_len);
+}
+
+fn pad_with_zeros_v3(v: &mut Vec<usize>, desired_len: usize) {
+    unimplemented!()
+    // debug_assert_eq!(result.len(), desired_len);
+}
+
+#[test]
+fn test_pad_twice_v1() {
+    let v = vec![1];
+    let v = pad_with_zeros_v1(v, 2);
+    let v = pad_with_zeros_v1(v, 4);
+    assert_eq!(v, vec![1, 0, 0, 0]);
+}
+
+#[test]
+fn test_pad_twice_v2() {
+    let v = vec![1];
+    let v = pad_with_zeros_v2(&v, 2);
+    let v = pad_with_zeros_v2(&v, 4);
+    assert_eq!(v, vec![1, 0, 0, 0]);
+}
+
+#[test]
+fn test_pad_twice_v3() {
+    let mut v = vec![1];
+    pad_with_zeros_v3(&mut v, 2);
+    pad_with_zeros_v3(&mut v, 4);
+    assert_eq!(v, vec![1, 0, 0, 0]);
+}
+
+/*
+    Problem 7: Move semantics continued
+
+    Write a function which appends a row to a vector of vectors.
+    Notice that it takes ownership over the row.
+    You shouldn't need to use .clone().
+
+    Why is this more general than being passed a &[bool]
+    and cloning it?
+
+    Second, write a function which returns whether
+    a row equals the first row in the vector of vectors.
+    Notice that it does not take ownership over the row.
+
+    Why is this more general than being passed a Vec<bool>?
+*/
+
+fn append_row(grid: &mut Vec<Vec<bool>>, row: Vec<bool>) {
+    unimplemented!()
+}
+
+fn is_first_row(grid: &[Vec<bool>], row: &[bool]) -> bool {
+    // Check if row is the first row in grid
+    // Remember to handle the case when grid is empty
+}
+
+/*
+    Problem 8: Modifying while iterating
+
+    In C and C++, you run into subtle bugs if you try to modify a data
+    structure while iterating over it. Rust's move semantics prevents that.
+*/
 
 use std::collections::HashMap;
 
-// Implement the following function which converts pairs from a vector,
+// To familiarize yourself with HashMaps,
+// implement the following function which converts pairs from a slice
 // into key-value pairs in a hashmap.
+// Documentation:
+// https://doc.rust-lang.org/std/collections/struct.HashMap.html
 
-fn vector_to_hashmap(v: &Vec<(i32, String)>) -> HashMap<i32, String> {
+fn vector_to_hashmap(v: &[(i32, String)]) -> HashMap<i32, String> {
     unimplemented!();
 }
 
-// Rust prevents us from deleting entries while iterating... Rewrite
-// this function to delete all entries in hashmap where the keys
+// Now rewrite this function to delete all entries in hashmap where the keys
 // are negative.
 fn delete_negative_keys(h: &mut HashMap<i32, i32>) {
     // This fails, uncomment to see error.
@@ -110,13 +221,28 @@ fn delete_negative_keys(h: &mut HashMap<i32, i32>) {
     // }
 }
 
-// For all entries in `add`: (k, v)
-// If `k` exists in `merged`, append `v` to the value of `merged[k]`.
-// If that `k` doesn't exist in `merged`, add the (k, v) to `merged`.
+/*
+    Problem 9: The Entry API
 
-// Use the Entry API:
-// https://doc.rust-lang.org/std/collections/hash_map/enum.Entry.html
-// Use `or_insert` and `and_modify`.
-fn merge_maps(merged: &mut HashMap<String, String>, add: HashMap<String,String>) {
+    Move semantics present interesting API design choices not found in other
+    languages.
+    HashMap is an example of such a API.
+    Specifically, the Entry API:
+    https://doc.rust-lang.org/std/collections/hash_map/enum.Entry.html
+
+    This allows for efficient HashMap access because we only access
+    the entry in the map (computing an expensive hash function) once.
+
+    Implement a function which does the following:
+        For all entries in `add`: (k, v)
+        If `k` exists in `merged`, append `v` to the value of `merged[k]`.
+        If that `k` doesn't exist in `merged`, add the (k, v) to `merged`.
+    Use `or_insert` and `and_modify`.
+*/
+
+fn merge_maps(
+    merged: &mut HashMap<String, String>,
+    add: HashMap<String,String>
+) {
     unimplemented!()
 }
